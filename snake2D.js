00d47a72620767fa282fc129bf8c177a9124007b
Shalin Patel <shalin617@gmail.com>
2015-04-12 16:50:36 -0400

    created snake object and rendered canvas

--- snake2D.js
+++ snake2D.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
   var ctx = canvas.getContext("2d");
   var w = $("#canvas").width();
   var h = $("#canvas").height();
+
+  //create snake object
+  var snake_array;
+
+  function create_snake(){
+    var length=5;
+    snake_array[];
+    for(var i = length-1; i>=0;i--){
+      snake_array.push({x:i,y=0});
+    }
+  }
+  create_snake();
+
+  function paint(){
+    //render canvas
+    ctx.fillStyle="white";
+    ctx.fillRect(0,0,w,h);
+    ctx.strokeStyle="black";
+    ctx.strokeRect(0,0,w,h);
+  }
 });
